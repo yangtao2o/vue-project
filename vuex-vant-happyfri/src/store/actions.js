56e18d203c9034f8e67186ae65c7027ce877efb6
yangtao <istaotao@aliyun.com>
2020-05-29 16:00:44 +0800

    feat: add store for vuex

---
+++ vuex-vant-happyfri/src/store/actions.js
@@ -0,0 +1,13 @@
+export default {
+  addNum({ commit, state }, id) {
+    // 点击下一题，记录答案id
+    commit('REMBER_ANSWER', id)
+    // 判断是否是最后一题，如果不是则跳转下一题
+    if (state.itemNum < state.itemDetail.length) {
+      commit('ADD_ITEMNUM', 1)
+    }
+  },
+  initializeData({ commit }) {
+    commit('INITIALIZEDATA')
+  }
+}
